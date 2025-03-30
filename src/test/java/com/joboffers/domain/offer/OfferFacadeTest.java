b6bc8a937a66d147178bb670133782d8b3688fa9
Oskar <oskar.pala92@gmail.com>
2025-03-30 17:06:50 +0200

    added unit test for fetching offers from remote and save only when url is unique in db

--- src/test/java/com/joboffers/domain/offer/OfferFacadeTest.java
+++ src/test/java/com/joboffers/domain/offer/OfferFacadeTest.java
@@ -1,5 +1,6 @@
 package com.joboffers.domain.offer;
 
+import com.joboffers.domain.offer.dto.JobOfferResponse;
 import com.joboffers.domain.offer.dto.OfferRequestDto;
 import com.joboffers.domain.offer.dto.OfferResponseDto;
 import org.junit.jupiter.api.Test;
@@ -11,46 +12,73 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class OfferFacadeTest {
     @Test
-    public void should_fetch_jobs_from_remote_and_save_all_offers_when_repository_is_empty(){
+    public void should_fetch_jobs_from_remote_and_save_all_offers_when_repository_is_empty() {
         //given
         OfferFacade offerFacade = new OfferFacadeTestConfiguration().offerFacadeForTests();
         assertThat(offerFacade.findAllOffers().isEmpty());
         //when
-        List<OfferResponseDto> result= offerFacade.fetchAllOffersAndSaveAllIfNotExists();
+        List<OfferResponseDto> result = offerFacade.fetchAllOffersAndSaveAllIfNotExists();
         //then
         assertThat(result).hasSize(6);
 
     }
+
     @Test
     public void should_save_3_offers_when_database_is_empty() {
         //given
         OfferFacade offerFacade = new OfferFacadeTestConfiguration().offerFacadeForTests();
         assertThat(offerFacade.findAllOffers()).isEmpty();
         //when
-        offerFacade.saveOffer(new OfferRequestDto("asd","qwe","123","asd1"));
-        offerFacade.saveOffer(new OfferRequestDto("asd","qwe","123","asd2"));
-        offerFacade.saveOffer(new OfferRequestDto("asd","qwe","123","asd3"));
+        offerFacade.saveOffer(new OfferRequestDto("asd", "qwe", "123", "asd1"));
+        offerFacade.saveOffer(new OfferRequestDto("asd", "qwe", "123", "asd2"));
+        offerFacade.saveOffer(new OfferRequestDto("asd", "qwe", "123", "asd3"));
         List<OfferResponseDto> result = offerFacade.findAllOffers();
         //then
         assertThat(result).hasSize(3);
 
     }
-    @Test
-    public void should_save_only_2_offers_when_repository_had_4_added_with_offer_urls() {
 
+    @Test
+    public void should_save_only_2_offers_when_repository_had_3_added_with_unique_offer_urls() {
+        //given
+        OfferFacade offerFacade = new OfferFacadeTestConfiguration(
+                List.of(
+                        new JobOfferResponse("asd", "qwe", "123", "asd1"),
+                        new JobOfferResponse("asd", "qwe", "123", "asd2"),
+                        new JobOfferResponse("asd", "qwe", "123", "asd3"),
+                        new JobOfferResponse("Junior", "company 1", "123", "https://jobboard.pl/1"),
+                        new JobOfferResponse("Java", "company 2", "123", "https://jobboard.pl/2")
+                )
+        ).offerFacadeForTests();
+        offerFacade.saveOffer(new OfferRequestDto("asd", "qwe", "123", "asd1"));
+        offerFacade.saveOffer(new OfferRequestDto("asd", "qwe", "123", "asd2"));
+        offerFacade.saveOffer(new OfferRequestDto("asd", "qwe", "123", "asd3"));
+        assertThat(offerFacade.findAllOffers()).hasSize(3);
+        //when
+        List<OfferResponseDto> response = offerFacade.fetchAllOffersAndSaveAllIfNotExists();
+        //then
+        assertThat(List.of(
+                        response.get(0).offerUrl(),
+                        response.get(1).offerUrl()
+                )
+        ).containsExactlyInAnyOrder("https://jobboard.pl/1", "https://jobboard.pl/2");
     }
+
     @Test
     public void should_throw_duplicate_key_exception_when_with_offer_url_exist() {
 
     }
+
     @Test
     public void should_throw_not_found_exception_when_offer_not_found() {
 
     }
+
     @Test
     public void should_fetch_from_jobs_from_remote_and_save_all_offers_when_repository_is_empty() {
 
     }
+
     @Test
     public void should_find_offer_by_id_when_offer_was_saved() {
 
