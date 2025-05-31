b7653b8b2d1dca3198b98ae6c4bdd7afd1894a41
Oskar <oskar.pala92@gmail.com>
2025-05-31 09:18:10 +0200

    added MongoRepository to OfferRepository fixed unit tests

--- src/main/java/com/joboffers/domain/offer/OfferFacadeConfiguration.java
+++ src/main/java/com/joboffers/domain/offer/OfferFacadeConfiguration.java
@@ -3,43 +3,11 @@ package com.joboffers.domain.offer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
-import java.util.Optional;
 @Configuration
 public class OfferFacadeConfiguration {
     @Bean
-    OfferFacade offerFacade(OfferFetchable offerFetchable){
-        OfferRepository repo = new OfferRepository() {
-            @Override
-            public boolean existsByOfferUrl(final String offerUrl) {
-                return false;
-            }
-            @Override
-            public Optional<Offer> findByOfferUrl(String offerUrl) {
-                return Optional.empty();
-            }
-
-            @Override
-            public List<Offer> findAll() {
-                return null;
-            }
-
-            @Override
-            public Offer save(final Offer offer) {
-                return null;
-            }
-
-            @Override
-            public List<Offer> saveAll(final List<Offer> offers) {
-                return null;
-            }
-
-            @Override
-            public Optional<Offer> findById(final String id) {
-                return Optional.empty();
-            }
-        };
-        OfferService offerService = new OfferService(repo,offerFetchable);
-        return new OfferFacade(repo,offerService);
+    OfferFacade offerFacade(OfferFetchable offerFetchable, OfferRepository repository) {
+        OfferService offerService = new OfferService(repository, offerFetchable);
+        return new OfferFacade(repository, offerService);
     }
 }
--- src/test/java/com/joboffers/domain/offer/InMemoryOfferRepository.java
+++ src/test/java/com/joboffers/domain/offer/InMemoryOfferRepository.java
@@ -1,10 +1,18 @@
 package com.joboffers.domain.offer;
 
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery;
+
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.stream.StreamSupport;
 
 public class InMemoryOfferRepository implements OfferRepository {
     Map<String, Offer> database = new ConcurrentHashMap<>();
@@ -19,17 +27,17 @@ public class InMemoryOfferRepository implements OfferRepository {
     }
 
     @Override
-    public Optional<Offer> findByOfferUrl(final String offerUrl) {
-        return Optional.of(database.get(offerUrl));
+    public Optional<Offer> findById(final String id) {
+        return Optional.ofNullable(database.get(id));
     }
 
     @Override
-    public List<Offer> findAll() {
-        return database.values().stream().toList();
+    public boolean existsById(final String s) {
+        return false;
     }
 
     @Override
-    public Offer save(Offer entity) {
+    public <S extends Offer> S save(S entity) {
         if (database.values().stream().anyMatch(offer -> offer.offerUrl().equals(entity.offerUrl()))) {
             throw new OfferDuplicateException(entity.offerUrl());
         }
@@ -42,18 +50,109 @@ public class InMemoryOfferRepository implements OfferRepository {
                 entity.offerUrl()
         );
         database.put(id.toString(), offer);
-        return offer;
+        return (S) offer;
     }
 
+
     @Override
-    public List<Offer> saveAll(List<Offer> offers) {
-        return offers.stream()
+    public <S extends Offer> List<S> saveAll(final Iterable<S> entities) {
+        return StreamSupport.stream(entities.spliterator(), false)
                 .map(this::save)
                 .toList();
     }
 
     @Override
-    public Optional<Offer> findById(final String id) {
-        return Optional.ofNullable(database.get(id));
+    public List<Offer> findAll() {
+        return database.values().stream().toList();
+    }
+
+    @Override
+    public Iterable<Offer> findAllById(final Iterable<String> strings) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(final String s) {
+
+    }
+
+    @Override
+    public void delete(final Offer entity) {
+
+    }
+
+    @Override
+    public void deleteAllById(final Iterable<? extends String> strings) {
+
+    }
+
+    @Override
+    public void deleteAll(final Iterable<? extends Offer> entities) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    @Override
+    public List<Offer> findAll(final Sort sort) {
+        return List.of();
+    }
+
+    @Override
+    public Page<Offer> findAll(final Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public <S extends Offer> S insert(final S entity) {
+        return null;
+    }
+
+    @Override
+    public <S extends Offer> List<S> insert(final Iterable<S> entities) {
+        return List.of();
+    }
+
+    @Override
+    public <S extends Offer> Optional<S> findOne(final Example<S> example) {
+        return Optional.empty();
+    }
+
+    @Override
+    public <S extends Offer> List<S> findAll(final Example<S> example) {
+        return List.of();
+    }
+
+    @Override
+    public <S extends Offer> List<S> findAll(final Example<S> example, final Sort sort) {
+        return List.of();
+    }
+
+    @Override
+    public <S extends Offer> Page<S> findAll(final Example<S> example, final Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public <S extends Offer> long count(final Example<S> example) {
+        return 0;
+    }
+
+    @Override
+    public <S extends Offer> boolean exists(final Example<S> example) {
+        return false;
+    }
+
+    @Override
+    public <S extends Offer, R> R findBy(final Example<S> example, final Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction) {
+        return null;
     }
 }
