063735e6a0e6efe9adbcb95ad9d9472662b40da1
Oskar <oskar.pala92@gmail.com>
2025-05-31 09:52:38 +0200

    added integration test for repository

--- src/integration/java/com/joboffers/features/TypicalScenarioUserWantToSeeOffersIntegrationTest.java
+++ src/integration/java/com/joboffers/features/TypicalScenarioUserWantToSeeOffersIntegrationTest.java
@@ -3,10 +3,15 @@ package com.joboffers.features;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.joboffers.BaseIntegrationTest;
 import com.joboffers.SampleJobOffersResponse;
+import com.joboffers.domain.offer.dto.OfferResponseDto;
 import com.joboffers.infrastructure.offer.scheduler.HttpOffersScheduler;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+import java.util.List;
 
 public class TypicalScenarioUserWantToSeeOffersIntegrationTest extends BaseIntegrationTest implements SampleJobOffersResponse {
 
@@ -21,12 +26,14 @@ public class TypicalScenarioUserWantToSeeOffersIntegrationTest extends BaseInteg
                 .willReturn(WireMock.aResponse()
                         .withStatus(HttpStatus.OK.value())
                         .withHeader("Content-Type", "application/json")
-                        .withBody(bodyWithFourOffersJson())));
-
+                        .withBody(bodyWithZeroOffersJson())));
 
 
         //step 2: scheduler ran 1st time and made GET to external server and system added 0 offers to database
-        httpOffersScheduler.fetchAllOffersAndSaveAllIfNotExists();
+//        given && when
+        List<OfferResponseDto> newOffers = httpOffersScheduler.fetchAllOffersAndSaveAllIfNotExists();
+//then
+        assertThat(newOffers).isEmpty();
         //step 3: user tried to get JWT token by requesting POST /token with username=someUser, password=somePassword and system returned UNAUTHORIZED(401)
         //step 4: user made GET /offers with no jwt token and system returned UNAUTHORIZED(401)
         //step 5: user made POST /register with username=someUser, password=somePassword and system registered user with status OK(200)
