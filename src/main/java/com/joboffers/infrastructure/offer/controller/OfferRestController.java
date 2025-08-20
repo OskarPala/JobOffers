e745ec65ce1391f636587ba9ef2fdd3013877fea
Oskar <oskar.pala92@gmail.com>
2025-08-20 16:12:04 +0200

    added new endpoint for getting offer by id

--- src/main/java/com/joboffers/infrastructure/offer/controller/OfferRestController.java
+++ src/main/java/com/joboffers/infrastructure/offer/controller/OfferRestController.java
@@ -5,6 +5,7 @@ import com.joboffers.domain.offer.dto.OfferResponseDto;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,10 @@ class OfferRestController {
     public ResponseEntity<List<OfferResponseDto>> findAllOffers() {
         List<OfferResponseDto> allOffers = offerFacade.findAllOffers();
         return ResponseEntity.ok(allOffers);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<OfferResponseDto> getOfferById(@PathVariable String id){
+        OfferResponseDto offerById = offerFacade.findOfferById(id);
+        return ResponseEntity.ok(offerById);
     }
 }
