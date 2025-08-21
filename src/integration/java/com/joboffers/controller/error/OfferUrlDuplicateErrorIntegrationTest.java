a174f90b79930a90afaef93b262524b7a444a351
Oskar <oskar.pala92@gmail.com>
2025-08-21 20:02:23 +0200

    added Integration test for duplicate key exception

--- src/integration/java/com/joboffers/controller/error/OfferUrlDuplicateErrorIntegrationTest.java
+++ src/integration/java/com/joboffers/controller/error/OfferUrlDuplicateErrorIntegrationTest.java
@@ -1,4 +1,44 @@
 package com.joboffers.controller.error;
 
-public class OfferUrlDuplicateErrorIntegrationTest {
+import com.joboffers.BaseIntegrationTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+public class OfferUrlDuplicateErrorIntegrationTest extends BaseIntegrationTest {
+    @Test
+    public void should_return_409_conflict_when_added_second_offer_with_same_offer_url() throws Exception {
+//step1
+//given & when
+        ResultActions perform1 = mockMvc.perform(post("/offers")
+                .content("""
+                        {
+                        "companyName": "company",
+                        "position": "position",
+                        "salary": "5 000 - 8 000 PLN",
+                        "offerUrl": "https://offers.pl/offer/1234"
+                        }
+                        """)
+                .contentType(MediaType.APPLICATION_JSON + ";charset=UTF-8"));
+//then
+        perform1.andExpect(status().isCreated());
+//step2
+//given && when
+        ResultActions perform2 = mockMvc.perform(post("/offers")
+                .content("""
+                        {
+                        "companyName": "company",
+                        "position": "position",
+                        "salary": "5 000 - 8 000 PLN",
+                        "offerUrl": "https://offers.pl/offer/1234"
+                        }
+                        """)
+                .contentType(MediaType.APPLICATION_JSON + ";charset=UTF-8"));
+//then
+        perform2.andExpect(status().isConflict());
+    }
 }
