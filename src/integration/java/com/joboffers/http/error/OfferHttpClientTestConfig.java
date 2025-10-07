8fe7768ccb0f44cb82b9b3ad5a7da132757f7ee7
OskarPala <oskar.pala92@gmail.com>
2025-10-07 20:18:17 +0200

    added integration error test for http client

--- src/integration/java/com/joboffers/http/error/OfferHttpClientTestConfig.java
+++ src/integration/java/com/joboffers/http/error/OfferHttpClientTestConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.client.RestTemplate;
 import static com.joboffers.BaseIntegrationTest.WIRE_MOCK_HOST;
 
 public class OfferHttpClientTestConfig extends OfferHttpClientConfig {
-    public OfferFetchable remoteOfferTestConfiguration(int port, int connectionTimeout, int readTimeout) {
+    public OfferFetchable remoteOfferTestClient(int port, int connectionTimeout, int readTimeout) {
         final RestTemplate restTemplate = restTemplate(connectionTimeout, readTimeout, restTemplateResponseErrorHandler());
         return remoteOfferClient(restTemplate, WIRE_MOCK_HOST, port);
     }
